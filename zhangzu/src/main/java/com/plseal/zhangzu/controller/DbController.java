4904358b13e0248e09be47fd26e4e56e2dc2d9ea
plseal <ttml0122@gmail.com>
2020-07-25 22:23:34 +0900

    return map

--- zhangzu/src/main/java/com/plseal/zhangzu/controller/DbController.java
+++ zhangzu/src/main/java/com/plseal/zhangzu/controller/DbController.java
@@ -1,5 +1,6 @@
 package com.plseal.zhangzu.controller;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -13,38 +14,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class DbController {
-   private static final Logger logger = LoggerFactory.getLogger(DbController.class);
-   @Autowired
-   JdbcTemplate jdbcTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(DbController.class);
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-   @RequestMapping(path="/users", method=RequestMethod.GET)
-   public String index() {
-       List<Map<String,Object>> list;
-       list = jdbcTemplate.queryForList("select * from face.employee");
-       return list.toString();
-   }
-
-   @RequestMapping(path="/users/{id}", method=RequestMethod.GET)
-   public String read(@PathVariable final String id) {
-       List<Map<String,Object>> list;
-       list = jdbcTemplate.queryForList("select * from face.employee where employee_id = ?", id);
-       return list.toString();
-   }
-   @RequestMapping(path="/get_one_zhangzu",method=RequestMethod.GET)
-   public String get_one_zhangzu(
-            @RequestParam("flg") final String flg,
-            @RequestParam("z_date") final String z_date,
-            @RequestParam("z_name") final String z_name)  {
-        
-        
-        List<Map<String,Object>> list;
-        
-        list = jdbcTemplate.queryForList("SELECT * FROM t_zhangzu WHERE z_date like CONCAT('%',?,'%') and z_name like CONCAT('%',?,'%') order by z_date desc ", z_date,z_name);
+    @RequestMapping(path = "/users", method = RequestMethod.GET)
+    public String index() {
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from face.employee");
         return list.toString();
+    }
+
+    @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
+    public String read(@PathVariable final String id) {
+        List<Map<String, Object>> list;
+        list = jdbcTemplate.queryForList("select * from face.employee where employee_id = ?", id);
+        return list.toString();
+    }
+
+    @RequestMapping(path = "/get_one_zhangzu", method = RequestMethod.GET)
+    public Map<String, Object> get_one_zhangzu(@RequestParam("flg") final String flg, @RequestParam("z_date") final String z_date,
+            @RequestParam("z_name") final String z_name) {
+
+        List<Map<String, Object>> list;
+
+        list = jdbcTemplate.queryForList(
+                "SELECT * FROM t_zhangzu WHERE z_date like CONCAT('%',?,'%') and z_name like CONCAT('%',?,'%') order by z_date desc ",
+                z_date, z_name);
+        // Map<String, String> resultJson = Collections.singletonMap("result", "OK");
+        logger.info("list.size():"+list.size());
+        logger.info("list.get(0):"+list.get(0));
+        Map<String, Object> oneMap = (Map<String, Object>)list.get(0);
+        // logger.info("oneMap.get(z_date):"+oneMap.get("z_date"));
+        // if(list.size() > 0) {
+            
+        //     resultJson.put("z_date",oneMap.get("z_date").toString());
+        // } else {
+        //     resultJson = Collections.singletonMap("result", "NG");
+        // }
+        // logger.info("resultJson:"+resultJson.toString());
+        return oneMap;
+        
+        
+
+        //return list.toString();
 
    }
 }
