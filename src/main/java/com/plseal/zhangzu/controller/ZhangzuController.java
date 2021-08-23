c9411865dc2eac684047787d49bb2d3d2e3a8fd4
plseal <plseal@163.com>
2021-08-23 19:53:47 +0900

    update complete

--- src/main/java/com/plseal/zhangzu/controller/ZhangzuController.java
+++ src/main/java/com/plseal/zhangzu/controller/ZhangzuController.java
@@ -1,37 +1,30 @@
 package com.plseal.zhangzu.controller;
 
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import com.plseal.zhangzu.common.master;
+import com.plseal.zhangzu.entity.Zhangzu;
+import com.plseal.zhangzu.entity.ZhangzuAnalysis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
-import java.util.Map;
-import com.plseal.zhangzu.entity.Zhangzu;
-import com.plseal.zhangzu.entity.ZhangzuAnalysis;
-import com.plseal.zhangzu.common.master;
-
-import java.util.Arrays;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.math.BigInteger;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 /**
  * @Description 
@@ -48,16 +41,13 @@ public class ZhangzuController {
 
 	@RequestMapping(path = "/index", method = RequestMethod.GET)
 	public String index(Model model) throws Exception {
-		//List<String> list = Arrays.asList("aa", "bb", "cc");
-		List<Map<String, Object>> list;
-        list = jdbcTemplate.queryForList("SELECT * FROM t_zhangzu WHERE z_date like '2021%' order by z_date desc ");
-        // Map<String, String> resultJson = Collections.singletonMap("result", "OK");
-        logger.info("list.size():"+list.size());
-        logger.info("list.get(0):"+list.get(0));
 
-		model.addAttribute("list", list);
-		model.addAttribute("msg", "Hello World zhangzu!!!");
-		
+		String sql = "SELECT * FROM t_zhangzu WHERE z_date like '2021%' order by z_date desc ";
+
+		RowMapper<Zhangzu> rowMapper = new BeanPropertyRowMapper<Zhangzu>(Zhangzu.class);
+        List<Zhangzu> list_zhangzu = jdbcTemplate.query(sql, rowMapper);
+
+		model.addAttribute("list_zhangzu", list_zhangzu);
 		return "index";
 	}
 
@@ -93,14 +83,49 @@ public class ZhangzuController {
 		logger.info("insert_post() z_date:" + z_date);
 		String sql = "INSERT INTO t_zhangzu VALUES(null,?,?,?,?,?,?,?)";
 		jdbcTemplate.update(sql,z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount);
-		
-		List<Map<String, Object>> list;
-        list = jdbcTemplate.queryForList("SELECT * FROM t_zhangzu WHERE z_date = ? order by z_date desc ",z_date);
-        
-        logger.info("list.size():"+list.size());
-        logger.info("list.get(0):"+list.get(0));
 
-		model.addAttribute("list", list);
+		String z_date_month = z_date.substring(0,7);
+		logger.info("z_date_month:" + z_date_month);
+
+		String sql_index = "SELECT * FROM t_zhangzu WHERE z_date like '" + z_date_month + "%' order by z_date desc ";
+        
+		RowMapper<Zhangzu> rowMapper = new BeanPropertyRowMapper<Zhangzu>(Zhangzu.class);
+        List<Zhangzu> list_zhangzu = jdbcTemplate.query(sql_index, rowMapper);
+
+		model.addAttribute("list_zhangzu", list_zhangzu);
+		return "index";
+	}
+
+	@PostMapping(path = "/update_delete_post")
+	public String update_delete_post(Model model,
+		@RequestParam("z_date") String z_date,
+		@RequestParam("z_name") String z_name,
+		@RequestParam("z_amount") BigInteger z_amount,
+		@RequestParam("z_type") String z_type,
+		@RequestParam("z_io_div") String z_io_div,
+		@RequestParam("z_remark") String z_remark,
+		@RequestParam("z_m_amount") BigInteger z_m_amount,
+		@RequestParam("id") String id,
+		@RequestParam("update_delete_button") String update_delete_button
+		) throws Exception {
+		logger.info("update_delete_post() update_delete_button:" + update_delete_button);
+
+		if("UPDATE".equals(update_delete_button)) {
+			String sql_update = "update t_zhangzu set z_date = ?,z_name = ?,z_amount = ?,z_type = ?,z_io_div = ?,z_remark = ?,z_m_amount = ? where id = ?";
+			jdbcTemplate.update(sql_update,z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount,id);
+		} else {
+			String sql_delete = "delete from t_zhangzu where id = ?";
+			jdbcTemplate.update(sql_delete,id);
+		}
+		String z_date_month = z_date.substring(0,7);
+		logger.info("z_date_month:" + z_date_month);
+		String sql_index = "SELECT * FROM t_zhangzu WHERE z_date like '"+z_date_month+"%' order by z_date desc ";
+        
+		RowMapper<Zhangzu> rowMapper = new BeanPropertyRowMapper<Zhangzu>(Zhangzu.class);
+        List<Zhangzu> list_zhangzu = jdbcTemplate.query(sql_index, rowMapper);
+
+		model.addAttribute("list_zhangzu", list_zhangzu);
+
 		return "index";
 	}
 
@@ -111,9 +136,21 @@ public class ZhangzuController {
         list = jdbcTemplate.queryForList("SELECT * FROM t_zhangzu WHERE id = ? order by z_date desc ", id);
         logger.info("list.size():"+list.size());
         logger.info("list.get(0):"+list.get(0));
-
-		model.addAttribute("list", list);
+		Zhangzu zhangzu = new Zhangzu();
+		zhangzu.setId(Integer.valueOf(list.get(0).get("id").toString()));
+		zhangzu.setZ_date(list.get(0).get("z_date").toString());
+		zhangzu.setZ_name(list.get(0).get("z_name").toString());
+		zhangzu.setZ_amount(Integer.valueOf(list.get(0).get("z_amount").toString()));
+		zhangzu.setZ_type(list.get(0).get("z_type").toString());
+		zhangzu.setZ_io_div(list.get(0).get("z_io_div").toString());
+		zhangzu.setZ_remark(list.get(0).get("z_remark").toString());
+		zhangzu.setZ_m_amount(Integer.valueOf(list.get(0).get("z_m_amount").toString()));
+		model.addAttribute("zhangzu", zhangzu);
 		
+		List<String> z_type_list = master.make_ztype_list();
+		
+		model.addAttribute("z_type_list", z_type_list);
+
 		return "update";
 	}
 
