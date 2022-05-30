c0fc6fe959b5107f39e6e86e5838f259e8321760
plseal <plseal@163.com>
2022-05-30 15:22:24 +0900

    issue #9 analysis init html complete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,9 @@ INSERT INTO DEPT(NAME) VALUES ('Development');
 
 
 INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/01','マイナンバーカード再交付',1000,'日常用品','支出','',0);
-INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/02','超市',1690,'餐饮饮食','支出','李nanaco',0);
+INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/02','超市',2000,'餐饮饮食','支出','李nanaco',0);
+INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/03','超市',3000,'餐饮饮食','支出','李nanaco',0);
 INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/02','李买太阳伞，面膜，小手绢',3608,'李服饰','支出','ライフ信用卡',0);
+INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/25','宋工资',200000,'工资','收入','',0);
+INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/26','买货',3000,'餐饮饮食','买货','',4000);
+INSERT INTO li_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/02/02','超市',4000,'餐饮饮食','支出','李nanaco',0);
