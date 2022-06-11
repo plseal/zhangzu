ae6b45ebde2c281e9d43f4cf66211d900f8007e4
plseal <plseal@163.com>
2022-06-11 16:36:04 +0900

    issue #18 add select and bootstrap5

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,4 @@ INSERT INTO t_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount
 INSERT INTO t_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/02','甲油胶透色',3608,'甲油胶透色','支出','',0);
 INSERT INTO t_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/01/25','美甲收入',20000,'美甲收入','收入','',0);
 INSERT INTO t_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2022/02/02','甲油胶猫眼',4000,'甲油胶猫眼','支出','',0);
+INSERT INTO t_zhangzu(z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount) VALUES('2021/02/02','甲油胶猫眼',4000,'甲油胶猫眼','支出','',0);
