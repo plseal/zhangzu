670c45c073d0574e8c83b155c2be40a7afeb364a
plseal <plseal@163.com>
2022-07-02 16:09:44 +0900

    issue #38 fix 文字化け

--- run_prod.bat
+++ run_prod.bat
@@ -2,4 +2,6 @@ cd C:\GitHub\zhangzu\src\main\resources\
 del application.properties
 copy application.properties_prod application.properties
 cd C:\GitHub\zhangzu
+chcp 65001
+chcp
 mvn spring-boot:run
