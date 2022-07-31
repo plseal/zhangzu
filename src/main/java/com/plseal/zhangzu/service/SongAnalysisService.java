c045dc398ffc798fe5d71f89077300256955c756
plseal <ttml0122@gmail.com>
2022-07-31 09:09:34 +0900

    bug fix ac_year null pointer

--- src/main/java/com/plseal/zhangzu/service/SongAnalysisService.java
+++ src/main/java/com/plseal/zhangzu/service/SongAnalysisService.java
@@ -110,7 +110,7 @@ public class SongAnalysisService {
 	public List<ZhangzuAnalysis> get_distinct_ac_year() throws Exception {
 		ZhangzuAnalysis zz_analysis;
 		List<ZhangzuAnalysis> list_result = new ArrayList<ZhangzuAnalysis>();
-		String strSQL3 = "select distinct left(z_date,4) as ac_year from t_zhangzu order by ac_year desc";
+		String strSQL3 = "select distinct left(z_date,4) as ac_year from t_zhangzu where z_date is not null order by ac_year desc";
 		logger.info("["+this.getClass()+"][get_distinct_ac_year][SQL3]"+strSQL3);
 		List<Map<String, Object>> list_tmp = jdbcTemplate.queryForList(strSQL3);
         logger.info("list.size():"+list_tmp.size());
