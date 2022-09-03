98eaa81c46fa36d4944349e2bc51505b22126f7f
plseal <plseal@163.com>
2022-09-03 17:05:25 +0900

    issue #55 li type change

--- src/main/java/com/plseal/zhangzu/common/LiMaster.java
+++ src/main/java/com/plseal/zhangzu/common/LiMaster.java
@@ -15,31 +15,31 @@ public class LiMaster {
 		z_type_list.add("甲油胶猫眼");
 		z_type_list.add("甲油胶晕染");
 		z_type_list.add("甲油胶玛卡龙");
-		z_type_list.add("功能胶（底胶）");
-		z_type_list.add("功能胶（免洗钢化封层）");
-		z_type_list.add("功能胶（防污镀晶封层）");
-		z_type_list.add("功能胶（磨砂封层）");
-		z_type_list.add("功能胶（俄式磨砂封层）");
-		z_type_list.add("功能胶（加固胶）");
-		z_type_list.add("功能胶（甲片粘合剂）");
-		z_type_list.add("功能胶（粘钻胶）");
-		z_type_list.add("功能胶（建构底胶）");
+		z_type_list.add("功能胶");
+		z_type_list.add("甲片粘合剂");
+		z_type_list.add("粘钻胶");
+		z_type_list.add("建构底胶");
 		z_type_list.add("美甲工具（光疗灯）");
 		z_type_list.add("美甲工具（手持灯）");
 		z_type_list.add("美甲工具（U型剪）");
 		z_type_list.add("美甲工具（甲片剪）");
 		z_type_list.add("美甲工具（卸钻钳）");
 		z_type_list.add("美甲工具（双头镊子）");
-		z_type_list.add("美甲工具（海绵搓条）");
-		z_type_list.add("美甲工具（卸甲搓条）");
-		z_type_list.add("美甲工具（打磨修型搓条）");
+		z_type_list.add("美甲工具（搓条）");
 		z_type_list.add("美甲工具（粉尘刷）");
 		z_type_list.add("美甲工具（印章）");
 		z_type_list.add("美甲工具（打磨机）");
 		z_type_list.add("美甲工具（打磨头）");
-		z_type_list.add("美甲工具（清洁水）");
+		z_type_list.add("美甲工具（清洁）");
 		z_type_list.add("美甲工具（卸甲包）");
-		z_type_list.add("饰品");
+		z_type_list.add("笔刷");
+		z_type_list.add("贴纸");
+		z_type_list.add("甲片");
+		z_type_list.add("胶带");
+		z_type_list.add("展示条");
+		z_type_list.add("脚甲用品");
+		
+		
 		
         return z_type_list;
     }
