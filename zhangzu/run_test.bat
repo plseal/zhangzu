d20cb6b3de0ed8c75b5f84fbcc0e69300d8d90be
plseal <plseal@163.com>
2022-10-23 15:43:50 +0900

    bat modify

--- zhangzu/run_test.bat
+++ zhangzu/run_test.bat
@@ -2,4 +2,5 @@ cd C:\work\GitHub\zhangzu\zhangzu\src\main\resources\
 del application.properties
 copy application.properties_test application.properties
 cd C:\work\GitHub\zhangzu\zhangzu
+echo "http://localhost:8080/h2-console"
 C:\maven\bin\mvn spring-boot:run
