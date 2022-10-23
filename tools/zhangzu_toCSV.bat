770b4646c7f057ca8eae547eedce405358c0c247
plseal <plseal@163.com>
2022-10-23 16:43:33 +0900

    modify bat

--- tools/zhangzu_toCSV.bat
+++ tools/zhangzu_toCSV.bat
@@ -1,3 +1,5 @@
+chcp 65001
+
 SET dbhost=localhost
 SET dbuser=root
 SET dbpasswd=123456
@@ -9,3 +11,5 @@ set sqlfile=zhangzu_toCSV.sql
 
 rem *******************************Get CSV from Target Table*****************************
 C:\mysql\bin\mysql -h%dbhost% -u%dbuser% -p%dbpasswd% %dbName% < %sqlpath%%sqlfile% --default-character-set=utf8 > C:\db_backup\t_zhangzu.csv
+
+dir c:\db_backup\t_zhangzu.csv
