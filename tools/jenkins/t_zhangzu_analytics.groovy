0609a5115d19b666ef21adfdd3578497a3e6d143
plseal <ttml0122@gmail.com>
2022-10-24 15:38:29 +0900

    analytics groovy

---
+++ tools/jenkins/t_zhangzu_analytics.groovy
@@ -0,0 +1,17 @@
+node('windows_agent'){
+    stage("step1. git checkout branch"){
+        bat """
+            cd c:\\Github\\zhangzu
+            dir
+            git fetch
+            git checkout feature/#60_csvtogcs
+            git pull
+        """
+    }
+    stage("step2. make csv from mysql"){
+        bat """
+            cd c:\\Github\\zhangzu\\tools
+            zhangzu_toCSV.bat
+        """
+    }
+}
