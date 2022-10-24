902dc1a95b6172b209c771a08876e8b16607e5e2
plseal <ttml0122@gmail.com>
2022-10-24 16:22:03 +0900

    python upload csv

--- tools/zhangzu_CSVtoGCS.py
+++ tools/zhangzu_CSVtoGCS.py
@@ -6,3 +6,13 @@ from google.cloud import storage
 storage_client  = storage.Client.from_service_account_json('c:\\tools\\gcpsecret.json')
 
 print("storage_client init OK!")
+
+
+bucket_name = 'uploadtogcs'
+file_name = 't_zhangzu.csv' #送るものの名前
+
+bucket = storage_client.get_bucket(bucket_name)
+blob = bucket.blob(file_name)
+blob.upload_from_filename(file_name)
+
+print("uploaded to 【project:family】 【GCS:uploadtogcs】 success!")
