6eaf10977b602f9de0a1d9c80a793f6382edf370
plseal <ttml0122@gmail.com>
2022-10-24 16:07:40 +0900

    add gcs operation by python

--- tools/zhangzu_CSVtoGCS.bat
+++ tools/zhangzu_CSVtoGCS.bat
@@ -5,3 +5,5 @@ dir c:\db_backup\t_zhangzu.csv
 python --version
 
 pip list
+
+python zhangzu_CSVtoGCS.py
---
+++ tools/zhangzu_CSVtoGCS.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+from io import BytesIO
+from google.cloud import storage
+
+# ↓my_gcp_credentials.jsonはサービスアカウントから発行している
+storage_client  = storage.Client.from_service_account_json('c:\\tools\\gcpsecret.json')
+
+print("storage_client init OK!")
