210ab441765143bc4fb74caced061db203df23f9
plseal <ttml0122@gmail.com>
2022-10-24 16:40:51 +0900

    zhangzu2022

--- tools/zhangzu_CSVtoGCS.py
+++ tools/zhangzu_CSVtoGCS.py
@@ -8,7 +8,7 @@ storage_client  = storage.Client.from_service_account_json('c:\\tools\\gcpsecret
 print("storage_client init OK!")
 
 
-bucket_name = 'uploadtogcs'
+bucket_name = 'zhangzu2022'
 file_name = 't_zhangzu.csv' #送るものの名前
 
 bucket = storage_client.get_bucket(bucket_name)
