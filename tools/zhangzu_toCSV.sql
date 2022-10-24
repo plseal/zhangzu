740e0e259c3a5487a0ac1e85472317102854f1b0
plseal <ttml0122@gmail.com>
2022-10-24 18:14:05 +0900

    add 2021

--- tools/zhangzu_toCSV.sql
+++ tools/zhangzu_toCSV.sql
@@ -1,1 +1,5 @@
-select z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount from t_zhangzu where z_date like '2022%' order by z_date;
+select z_date,z_name,z_amount,z_type,z_io_div,z_remark,z_m_amount from t_zhangzu 
+where 
+   z_date like '2022%' 
+or z_date like '2021%' 
+order by z_date;
