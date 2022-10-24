2ab23a187b2d36fbd426da7a59715807329b9c8f
plseal <ttml0122@gmail.com>
2022-10-24 14:21:45 +0900

    groovy stage1 stage2

--- tools/jenkins/t_zhangzu_backup.groovy
+++ tools/jenkins/t_zhangzu_backup.groovy
@@ -1,5 +1,17 @@
 node('windows_agent'){
-    stage("step1. do something"){
-        bat 'echo "hello"'
+    stage("step1. git checkout branch"){
+        bat """
+            cd c:\\Github\\zhangzu
+            dir
+            git fetch
+            git checkout feature/#58_add_backup.bat
+            git pull
+        """
+    }
+    stage("step2. zhangzu_backup"){
+        bat """
+            cd c:\\Github\\zhangzu\\tools
+            zhangzu_backup.bat
+        """
     }
 }
