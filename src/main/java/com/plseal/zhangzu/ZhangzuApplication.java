f91329dcb45c8f7c1a74c52405f040f0127e71da
plseal <plseal@163.com>
2022-12-05 11:41:21 +0900

    issue #70 save pid to file

--- src/main/java/com/plseal/zhangzu/ZhangzuApplication.java
+++ src/main/java/com/plseal/zhangzu/ZhangzuApplication.java
@@ -2,15 +2,14 @@ package com.plseal.zhangzu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ZhangzuApplication {
-	private static final Logger logger = LoggerFactory.getLogger(ZhangzuApplication.class);
 	public static void main(String[] args) {
-		logger.info("http://localhost:8080/get_one_zhangzu?flg=Select&z_date=2020/06/23&z_name=%E5%9C%B0%E9%93%81%E8%B6%85%E5%B8%82");
-		SpringApplication.run(ZhangzuApplication.class, args);
+		SpringApplication application = new SpringApplication(ZhangzuApplication.class);
+		application.addListeners(new ApplicationPidFileWriter("./app.pid"));
+		application.run(args);
 	}
 
 }
