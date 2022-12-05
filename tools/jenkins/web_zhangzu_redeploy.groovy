8e4f0d4281718bd7ec9e4ec8738a946ae46650a8
plseal <ttml0122@gmail.com>
2022-12-05 11:49:13 +0900

    first add web_zhangzu_redeploy.groovy

---
+++ tools/jenkins/web_zhangzu_redeploy.groovy
@@ -0,0 +1,27 @@
+node{
+    stage("step1. git checkout branch"){
+        bat """
+            cd c:\\Github\\zhangzu
+            dir
+            git fetch
+            git checkout master
+            git pull
+        """
+    }
+    stage("step2. stop SpringBoot"){
+        bat """
+            cd c:\\Github\\zhangzu
+            SET /P VALUE_FROM_FILE= < app.pid
+            taskkill /pid %VALUE_FROM_FILE% /f
+        """
+    }
+    stage("step3. start SpringBoot Prod"){
+        bat """
+            cd C:\GitHub\zhangzu\src\main\resources\
+            del application.properties
+            copy application.properties_prod application.properties
+            cd C:\GitHub\zhangzu
+            start mvn spring-boot:run
+        """
+    }
+}
