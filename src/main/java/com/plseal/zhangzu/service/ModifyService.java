37264090e3e0314824ee6c989254f5e6b815780a
plseal <ttml0122@gmail.com>
2023-01-07 05:55:08 +0000

    issue #82 bug fix

--- src/main/java/com/plseal/zhangzu/service/ModifyService.java
+++ src/main/java/com/plseal/zhangzu/service/ModifyService.java
@@ -22,7 +22,7 @@ public class ModifyService {
 	public List<Zhangzu> query_db_index(String table_id, String year) throws Exception {
         logger.info("query_db_index [start]");
 
-		String sql = "SELECT id,z_date,z_name,z_amount,z_type,z_io_div,z_remark,IFNULL(z_m_amount,0) as z_m_amount FROM " + table_id +" WHERE z_date like '" + year + "%' order by z_date desc ";
+		String sql = "SELECT id,z_date,z_name,IFNULL(z_amount,0),z_type,z_io_div,z_remark,IFNULL(z_m_amount,0) as z_m_amount FROM " + table_id +" WHERE z_date like '" + year + "%' order by z_date desc ";
         logger.info("query_db_index [sql]"+sql);
 		RowMapper<Zhangzu> rowMapper = new BeanPropertyRowMapper<Zhangzu>(Zhangzu.class);
         List<Zhangzu> list_zhangzu = jdbcTemplate.query(sql, rowMapper);
