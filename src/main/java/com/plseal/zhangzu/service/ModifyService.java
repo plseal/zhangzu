f7a0b96f61b2dbdab418d408656cd05652f3383e
plseal <ttml0122@gmail.com>
2023-01-07 06:32:56 +0000

    issue #84 bug fix2

--- src/main/java/com/plseal/zhangzu/service/ModifyService.java
+++ src/main/java/com/plseal/zhangzu/service/ModifyService.java
@@ -35,7 +35,7 @@ public class ModifyService {
      */
 	public List<Zhangzu>  query_db_for_update_html(String table_id, Integer id) throws Exception {
         logger.info("query_db_for_update_html [start]");
-		String sql = "SELECT id,z_date,z_name,z_amount,z_type,z_io_div,z_remark,IFNULL(z_m_amount,0) as z_m_amount FROM " + table_id +" WHERE id = " + id + " order by z_date desc ";
+		String sql = "SELECT id,z_date,z_name,IFNULL(z_amount,0),z_type,z_io_div,z_remark,IFNULL(z_m_amount,0) as z_m_amount FROM " + table_id +" WHERE id = " + id + " order by z_date desc ";
         logger.info("query_db_for_update_html [sql]"+sql);
         RowMapper<Zhangzu> rowMapper = new BeanPropertyRowMapper<Zhangzu>(Zhangzu.class);
         List<Zhangzu> list_zhangzu  = jdbcTemplate.query(sql, rowMapper);
